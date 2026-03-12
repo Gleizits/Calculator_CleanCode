fb40f2c5be27f80c6fb02c024d67560f8b7e14d9
Gleizits <Geissel.Espino@upsjb.edu.pe>
2026-03-12 11:21:35 -0500

    codigo terminado, toca test oño

--- main.py
+++ main.py
@@ -4,4 +4,47 @@ import libs.op_math as m
 # main function
 
 def main():
-    
+    while True:
+        try:
+            # print menu
+            print("1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n5. Exit")
+
+            # get user input menu
+            choice = int(input("Enter your choice: "))
+            
+            # 5 option
+            if choice == 5:
+                print("Goodbye!")
+                break
+
+            # if user dont enter 1-5
+            if choice not in [1, 2, 3, 4]:
+                print("Invalid choice. Please enter a number from 1 to 5.")
+                continue
+
+            # input number
+            num1 = float(input("Enter a number: "))
+            num2 = float(input("Enter another number: "))
+
+            # op maths
+            if choice == 1:
+                result = m.add(num1, num2)
+                print(f"The result of {num1} + {num2} is: {result}")
+
+            elif choice == 2:
+                result = m.subtract(num1, num2)
+                print(f"The result of {num1} - {num2} is: {result}")
+
+            elif choice == 3:
+                result = m.multiply(num1, num2)
+                print(f"The result of {num1} * {num2} is: {result}")
+
+            elif choice == 4:
+                result = m.divide(num1, num2)
+                print(f"The result of {num1} / {num2} is: {result}")
+
+        except ValueError:
+            print("Ops! Please enter a number.")
+
+if __name__ == "__main__":
+    main()
